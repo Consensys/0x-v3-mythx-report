c4ee969f548c4dc10d025a48cb474e2ee070e4bc
Bernhard Mueller <b-mueller@users.noreply.github.com>
2019-11-01 15:43:34 +0100

    Make FixedMath tests self-contained

--- code/VerifyFixedMath.sol
+++ code/VerifyFixedMath.sol
@@ -1,18 +1,48 @@
 /*
-
   Integer overflow test for LibFixedMath
   Use MythX "full" mode analysis to verify.
-
 */
 
 pragma solidity ^0.5.9;
 
-import "./LibFixedMath.sol";
-
-
 contract VerifyFixedMath {
 
-    function EnsureAddNoOverflow(
+    // Verify that _add, _mil and _div don't overflow
+
+    int256 private constant MIN_FIXED_VAL = int256(0x8000000000000000000000000000000000000000000000000000000000000000);
+
+ 
+    /// @dev Returns the multiplication two numbers, reverting on overflow.
+    function _mul(int256 a, int256 b) private pure returns (int256 c) {
+        if (a == 0) {
+            return 0;
+        }
+        c = a * b;
+        if (c / a != b) {
+            revert();
+        }
+    }
+
+    /// @dev Returns the division of two numbers, reverting on division by zero.
+    function _div(int256 a, int256 b) private pure returns (int256 c) {
+        if (b == 0) {
+            revert();
+        }
+        if (a == MIN_FIXED_VAL && b == -1) {
+            revert();
+        }
+        c = a / b;
+    }
+
+    /// @dev Adds two numbers, reverting on overflow.
+    function _add(int256 a, int256 b) private pure returns (int256 c) {
+        c = a + b;
+        if ((a < 0 && b < 0 && c > a) || (a > 0 && b > 0 && c < a)) {
+            revert();
+        }
+    }
+
+     function EnsureAddNoOverflow(
         // Sum of two positive numbers must be positive
         int256 a,
         int256 b
@@ -20,15 +50,10 @@ contract VerifyFixedMath {
         public
         returns (int256)
     {
-
         require(a >= 0);
         require(b > 0);
 
-        int256 result = LibFixedMath._add(a, b);
-
-        assert(result > 0);
-        
-        return result;
+        assert(_add(a, b) > 0);
     }
 
     function EnsureAddNoUnderflow(
@@ -39,15 +64,10 @@ contract VerifyFixedMath {
         public
         returns (int256)
     {
-
         require(a <= 0);
         require(b < 0);
 
-        int256 result = LibFixedMath._add(a, b);
-
-        assert(result < 0);
-        
-        return result;
+        assert(_add(a, b) <= 0);
     }
 
     function EnsureMulNoOverflow(
@@ -58,13 +78,10 @@ contract VerifyFixedMath {
         public
         returns (int256)
     {
-
         require(a >= 0);
         require(b > 0);
-
-        int256 result = LibFixedMath._mul(a, b);
         
-        assert(result >= 0);
+        assert(_mul(a, b) >= 0);
     }
 
     function EnsureMulNoUnderflow(
@@ -75,14 +92,12 @@ contract VerifyFixedMath {
         public
         returns (int256)
     {
-
         require(a < 0);
         require(b < 0);
-
-        int256 result = LibFixedMath._mul(a, b);
         
-        assert(result >= 0);
+        assert(_mul(a, b) > 0);
     }
+
     function EnsureDivNoOverflow( 
         // Result of dividing positive number by positive number must be positive
         int256 a,
@@ -91,13 +106,10 @@ contract VerifyFixedMath {
         public
         returns (int256)
     {
-
         require(a >= 0);
         require(b > 0);
-
-        int256 result = LibFixedMath._div(a, b);
         
-        assert(result >= 0);
+        assert(_div(a, b) >= 0);
     }
 
     function EnsureDivNoUnderflow(
@@ -108,13 +120,9 @@ contract VerifyFixedMath {
         public
         returns (int256)
     {
-
         require(a < 0);
         require(b < 0);
 
-        int256 result = LibFixedMath._div(a, b);
-        
-        assert(result >= 0);
+        assert(_div(a, b) > 0);
     }
-
 }
